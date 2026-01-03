f3013758006ae77d6e8dd02f322ebdd52558ef90
andrebarbato <94872081+andrebarbato@users.noreply.github.com>
2026-01-03 01:02:40 +0000

    Deploying to gh-pages from @ andrebarbato/andrebarbato.github.io@2397931f0fdbdcd9ab28b4f9e66d5c7dd8197690 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -497,25 +497,11 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/https://www.linkedin.com/in/andrebarbato/", "_blank");
         },
       },{
       id: 'light-theme',
